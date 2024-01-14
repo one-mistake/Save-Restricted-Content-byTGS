caa503f25ccaba0ae7acdf6f80deff7ed1580bf4
Vikas Chaurasia <bca2124146@smsvaranasi.in>
2024-01-14 21:35:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def progress(current, total, message, type):
 @bot.on_message(filters.command(["start"]))
 def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 	bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot by TGS, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/HackingCraze24_7d")]]), reply_to_message_id=message.id)
+	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🚀 Update Channel", url="https://t.me/HackingCraze24_7d")]]), reply_to_message_id=message.id)
 
 
 @bot.on_message(filters.text)
@@ -247,34 +247,11 @@ def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
 	except: pass
 
 
-USAGE = """**FOR PUBLIC CHATS**
+USAGE = """**🚀 SEND ME LINK OF ANY MESSAGE TO CLONE IT HERE, FOR PRIVATE CHANNEL MESSAGE, SEND INVITE LINK FIRST.**
 
-**__just send post/s link__**
+**FOR MULTIPLE POST AT A TIME :- /batch**
 
-**FOR PRIVATE CHATS**
-
-**__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__**
-
-**FOR BOT CHATS**
-
-**__send link with** '/b/', **bot's username and message id, you might want to install some unofficial client to get the id like below__**
-
-```
-https://t.me/b/botusername/4321
-```
-
-**MULTI POSTS**
-
-**__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__**
-
-```
-https://t.me/xxxx/1001-1010
-
-https://t.me/c/xxxx/101 - 120
-```
-
-**__note that space in between doesn't matter__**
+**❣️ POWERED BY :- @HackingCraze24_7d**
 """
 
 
